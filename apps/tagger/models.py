3fffbf8cbfc5731f6278847eb2ad2a2ecf73cd40
Faris Chebib <faris@zaneprep.com>
2014-01-17 13:57:55 -0700

    added initial models for discussions and tagger

--- apps/tagger/models.py
+++ apps/tagger/models.py
@@ -1,3 +1,14 @@
 #encoding: utf-8
 
 from django.db import models
+
+from coalio.models.mixins import TimestampMixin
+
+class TaggedPost(TimestampMixin):
+    ref_url = models.URLField(max_length=300)
+    content = models.TextField(blank=True)
+
+    tagged_by = models.ForeignKey('profiles.Coalio.User')
+
+    def __unicode__(self):
+        return "TaggedPost #%s from %s" % (self.id, self.ref_url)
